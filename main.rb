8384938b9714678da6689424846ef0a882e20314
va1t <Peter.Maxcy@gmail.com>
2014-10-03 17:56:20 -0400

    added card pictures

--- main.rb
+++ main.rb
@@ -5,6 +5,34 @@ require 'pry'
 set :sessions, true
 
 helpers do
+	def get_card_img(card)
+		#suits = ['H', 'D', 'C', 'S']
+		#values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+		
+		if card[0] == 'H'
+			suit = 'hearts'
+		elsif card[0] == 'D'
+			suit = 'diamonds'
+		elsif card[0] == 'C'
+			suit = 'clubs'
+		else 
+			suit = 'spades'
+		end
+		
+		if card[1] == 'J'
+			value = 'jack'
+		elsif card[1] == 'Q'
+			value = 'queen'
+		elsif card[1] == 'K'
+			value = 'king'
+		elsif card[1] == 'A'
+			value = 'ace'
+		else
+			value = card[1]
+		end
+		
+		str = suit + '_' + value
+	end
 
 end
 
@@ -46,6 +74,22 @@ post '/game' do
 	if params[:player_bet]
 		session[:player_bet] = params[:player_bet].to_i
 		session[:player_money] -= params[:player_bet].to_i
+		
+		#Create Deck
+		suits = ['H', 'D', 'C', 'S']
+		values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+		
+		#Suffle and Combine
+		session[:deck] = suits.product(values).shuffle!
+		session[:dealer_cards] = [] 
+		session[:player_cards] = []
+		
+		#Deal cards
+		session[:player_cards] << session[:deck].pop
+		session[:dealer_cards] << session[:deck].pop
+		session[:player_cards] << session[:deck].pop
+		session[:dealer_cards] << session[:deck].pop		
+		
 		erb :game		
 	else
 		redirect '/bet'
@@ -59,4 +103,13 @@ end
 get '/bet' do
 
 	erb :bet
+end
+
+post '/hit' do
+	session[:player_cards] << session[:deck].pop
+	redirect '/game'
+end
+
+post '/stay' do
+	redirect '/game'
 end
