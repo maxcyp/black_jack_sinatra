fd7efab4bfb5f058f964f1052fa6b261d8da1b25
va1t <Peter.Maxcy@gmail.com>
2014-10-03 11:50:01 -0400

    Next update

--- main.rb
+++ main.rb
@@ -5,14 +5,30 @@ require 'pry'
 set :sessions, true
 
 get '/' do
-	
-	"Hello World"
+	if !session[:player_name]
+		redirect '/new_game'
+	else
+		redirect '/game'
+	end
 end
 
-get '/test' do
-	erb :test
+get '/new_game' do
+	erb :new_game
 end
 
 get '/sswn/password' do
 	erb :"sswn/password"
+end
+
+post '/set_name' do
+	if !params[:player_name]
+		redirect '/new_game'
+	else
+		session[:player_name] = params[:player_name]
+		redirect '/play'
+	end
+end
+
+get '/game' do
+	erb :game
 end
